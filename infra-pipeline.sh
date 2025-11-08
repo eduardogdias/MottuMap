9857728cd37a640046c64040d5be60ab1febccfb
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-11-08 18:44:58 -0300

    Dockerfile para criar imagem no ACR

--- infra-pipeline.sh
+++ infra-pipeline.sh
@@ -1,28 +1,39 @@
 az provider register --namespace Microsoft.Sql
 az provider register --namespace Microsoft.Web
  
-export NAME="mottumap"
-export RG="rg-${NAME}"
-export LOCATION="brazilsouth"
+NAME="mottumap" 
+RG="rg-$NAME" 
+LOCATION="brazilsouth"
  
 # SQL Server e Banco
-export SERVER_NAME="sqlserver-${NAME}"
-export USERNAME="admsql"
-export PASSWORD="Fiap@2tdsvms"
-export DBNAME="db-${NAME}"
+SERVER_NAME="sqlserver-$NAME"
+USERNAME="admsql"
+PASSWORD="Fiap@2tdsvms"
+DBNAME="db-$NAME"
  
 #Plano e Web App
-export WEBAPP_NAME="webapp-${NAME}"
-export PLAN_NAME="plan-${NAME}"
-export RUNTIME="JAVA:17-java17"
+WEBAPP_NAME="webapp-$NAME"
+PLAN_NAME="plan-$NAME"
+RUNTIME="JAVA:17-java17"
+
+#ACI
+NOME_ACR="acr$NAME"
+skuACR=Basic
+
+
+echo "Criando RG"
+az group create --name $RG --location $LOCATION
+
+echo "Criando Server e Banco"
+az sql server create -l $LOCATION -g $RG -n $SERVER_NAME -u $USERNAME -p $PASSWORD --enable-public-network true &&
+az sql db create -g $RG -s $SERVER_NAME -n $DBNAME --service-objective Basic --backup-storage-redundancy Local --zone-redundant false &&
+az sql server firewall-rule create -g $RG -s $SERVER_NAME -n AllowAll --start-ip-address 0.0.0.0 --end-ip-address 255.255.255.255
  
-# Criando Server e Banco
-az group create --name "$RG" --location "$LOCATION" &&
-az sql server create -l "$LOCATION" -g "$RG" -n "$SERVER_NAME" -u "$USERNAME" -p "$PASSWORD" --enable-public-network true &&
-az sql db create -g "$RG" -s "$SERVER_NAME" -n "$DBNAME" --service-objective Basic --backup-storage-redundancy Local --zone-redundant false &&
-az sql server firewall-rule create -g "$RG" -s "$SERVER_NAME" -n AllowAll --start-ip-address 0.0.0.0 --end-ip-address 255.255.255.255
- 
-# Criando Plano e Web App
-az appservice plan create -n "$PLAN_NAME" -g "$RG" --location "$LOCATION" --sku F1 --is-linux  &&
-az webapp create -n "$WEBAPP_NAME" -g "$RG" -p "$PLAN_NAME" --runtime "$RUNTIME"
+echo "Criando ACI"
+az acr create --resource-group $RG --name $NOME_ACR --sku $skuACR
+az acr update --name $NOME_ACR --resource-group $RG --admin-enabled true
+
+echo "Criando Plano e Web App"
+az appservice plan create -n $PLAN_NAME -g $RG --location $LOCATION --sku F1 --is-linux  &&
+az webapp create -n $WEBAPP_NAME -g $RG -p $PLAN_NAME --runtime $RUNTIME
  
