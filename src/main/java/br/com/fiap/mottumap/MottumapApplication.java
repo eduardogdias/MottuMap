9df9a493b997033d285f908773fb24a685df4f95
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-11-08 12:44:10 -0300

    Alteração nome do projeto

--- src/main/java/br/com/fiap/mottumap/MottumapApplication.java
+++ src/main/java/br/com/fiap/mottumap/MottumapApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MottuMapApplication {
+public class MottumapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MottuMapApplication.class, args);
+		SpringApplication.run(MottumapApplication.class, args);
 
 	}
 	
